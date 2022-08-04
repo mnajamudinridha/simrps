739153f6c779dd7c6742b50c8bc1252e6fe46893
M. Najamudin Ridha <naja@Ms-MacBook-Pro.local>
2022-08-04 12:30:19 +0800

    perbaikan update rps

--- modul/userrps.php
+++ modul/userrps.php
@@ -125,7 +125,10 @@ function tampildata($con, $team, $prosesrps)
 
                 echo '<input type="hidden" name="rps" value="' . $rps['uuid'] . '">';
                 echo '<input type="hidden" name="team" value="' . $team . '">';
-                echo '</table></div>';
+                echo '</table></div><br>';
+                echo '<div class="table-responsive tabledatarps">';
+                tampiltabel($con, $rps['uuid']);
+                echo '</div>';
             } else {
                 echo '<tr><th>7.</th><th colspan=3>Deskripsi Singkat Mata Kuliah</th></tr>';
                 echo '<tr><td colspan=4>' . cetakHTML($rps['deskripsi']) . '</td></tr>';
